a64c46617e1c23013a5845d2915310fdc84ed403
KryDos <furyinbox@gmail.com>
2014-03-09 18:03:50 +0200

    add link to the top bar which follow at the statuses plugin

--- elgg/mod/levelstatus/start.php
+++ elgg/mod/levelstatus/start.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * get ElggUser object for current user
+ */
+$user_object = get_loggedin_user();
+
 function initialize_plugin() {
 
 }
+
 elgg_register_event_handler('init', 'system', 'initialize_plugin');
 
 elgg_register_action('levelstatus/save', elgg_get_plugins_path() . 'levelstatus/actions/levelstatus/save.php', 'admin');
 
 
 
+/**
+ * urls handler
+ */
 function levelstatus_page_handler($segments) {
     if($segments[0] == 'add') {
         include elgg_get_plugins_path() . 'levelstatus/pages/levelstatus/add.php';
@@ -21,4 +30,16 @@ function levelstatus_page_handler($segments) {
     return false;
 }
 elgg_register_page_handler('levelstatus', 'levelstatus_page_handler');
+
+
+
+# add menu to the top bar if is admin
+if($user_object->isAdmin()) {
+    elgg_register_menu_item('topbar', array(
+        'name' => 'levelstatus_top_link',
+        'href' => 'levelstatus/all',
+        'title' => 'List of statuses',
+        'text' => 'level (statuses) list'
+    ));
+}
 
