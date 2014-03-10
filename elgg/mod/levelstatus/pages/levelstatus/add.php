961dea0e438b2e1f770cbc38b03ca5fcc6d0d9c3
KryDos <furyinbox@gmail.com>
2014-03-10 17:59:33 +0200

    add links to the sideabr @levelstatus

--- elgg/mod/levelstatus/pages/levelstatus/add.php
+++ elgg/mod/levelstatus/pages/levelstatus/add.php
@@ -5,6 +5,11 @@
 gatekeeper();
 
 /**
+ * add item to the sidebar for geting list of all entities
+ */
+add_submenu_item('Show all statuses', 'levelstatus/all');
+
+/**
  * page title
  */
 $title = 'Add new Level';
@@ -18,5 +23,6 @@ $body = elgg_view_layout('one_sidebar', array(
     'content' => $content,
     'sidebar' => $sidebar
 ));
+
 
 echo elgg_view_page($title, $body);
--- elgg/mod/levelstatus/pages/levelstatus/all.php
+++ elgg/mod/levelstatus/pages/levelstatus/all.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * add item for adding new record to the sidebar
+ */
+add_submenu_item('Add new status', 'levelstatus/add');
+
 $body = elgg_list_entities(array(
     'type' => 'object',
     'subtype' => 'levelstatus'
 ));
 
-$body = elgg_view_layout('one_column', array('content'=>$body));
+$sidebar = '';
+
+$body = elgg_view_layout('one_sidebar', array(
+    'content'=>$body,
+    'sidebar'=>$sidebar
+));
 echo elgg_view_page('All Level Statuses', $body);
