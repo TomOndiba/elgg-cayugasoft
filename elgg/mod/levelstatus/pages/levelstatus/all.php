4c0bacb40e09ed2ec54e460d34406d9ca80dceae
KryDos <furyinbox@gmail.com>
2014-03-10 17:13:56 +0200

    fix typo in the all.php in the layout setting

--- elgg/mod/levelstatus/pages/levelstatus/all.php
+++ elgg/mod/levelstatus/pages/levelstatus/all.php
@@ -4,5 +4,5 @@ $body = elgg_list_entities(array(
     'subtype' => 'levelstatus'
 ));
 
-$body = elgg_view_layout('one_columnt', array('content'=>$body));
+$body = elgg_view_layout('one_column', array('content'=>$body));
 echo elgg_view_page('All Level Statuses', $body);
