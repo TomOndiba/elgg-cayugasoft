86496bf010eab948e5335a04d3154ed85193570c
KryDos <furyinbox@gmail.com>
2014-03-16 20:08:12 +0200

    add level_list view

--- elgg/mod/levelstatus/pages/levelstatus/all.php
+++ elgg/mod/levelstatus/pages/levelstatus/all.php
@@ -7,7 +7,7 @@ add_submenu_item('Add new status', 'levelstatus/add');
 $body = elgg_list_entities(array(
     'type' => 'object',
     'subtype' => 'levelstatus'
-));
+), 'elgg_get_entities', 'status_list_view');
 
 $sidebar = '';
 
@@ -16,3 +16,7 @@ $body = elgg_view_layout('one_sidebar', array(
     'sidebar'=>$sidebar
 ));
 echo elgg_view_page('All Level Statuses', $body);
+
+function status_list_view($entities, $vars, $offset, $limit, $full_view, $list_type_toggle, $pagination) {
+    return elgg_view('levelstatus/level_list', array('items' => $entities));
+}
