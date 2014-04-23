b7bd65849523e687074976c5ec46a05abbe3f44a
KryDos <furyinbox@gmail.com>
2014-04-24 00:15:03 +0300

    add a little bit comments

--- elgg/mod/levelstatus/pages/levelstatus/view.php
+++ elgg/mod/levelstatus/pages/levelstatus/view.php
@@ -5,23 +5,43 @@
 $status = get_entity(get_input('guid'));
 
 /**
- * create empty sidebar
+ * create an empty sidebar
  */
 $sidebar = '';
 
+/**
+ * registrate a new item in the sidebar
+ */
+add_submenu_item('Remove status', 'levelstatus/add');
+
+/**
+ * add title to the body
+ */
 $body = elgg_view_title($status->title);
 
+/**
+ * add description to the body
+ */
 $body .= elgg_view('output/longtext', array(
     'value' => $status->description,
 ));
 
+/**
+ * add tags
+ */
 $body .= elgg_view('output/tags', array(
     'tags' => $status->tags,
 ));
 
+/**
+ * create page with 'one_sidebar' layout
+ */
 $body = elgg_view_layout('one_sidebar', array(
     'content' => $body,
     'sidebar' => $sidebar
 ));
 
+/**
+ * show page
+ */
 echo elgg_view_page('Entity view', $body);
